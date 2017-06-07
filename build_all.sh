803c40f2d4a2d3d616b612e1201298f64bc4a199
Antony Pavlov <antonynpavlov@gmail.com>
2017-06-07 19:56:21 +0300

    WIP: build_all.sh
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- build_all.sh
+++ build_all.sh
@@ -42,9 +42,22 @@ build_quartus()
 	export PATH=$SAVE_PATH
 }
 
+build_quartus q13.0 /opt/altera/13.0/quartus/bin \
+	de1-picorv32-wb-soc
+
+exit
+
 build_quartus q16.0 /opt/altera/16.0/quartus/bin \
 	marsohod2bis-picorv32-wb-soc,marsohod3-picorv32-wb-soc,de1-soc-picorv32-wb-soc
 build_quartus q13.1 /opt/altera/13.1/quartus/bin \
 	marsohod2-picorv32-wb-soc,marsohod2bis-picorv32-wb-soc,de1-soc-picorv32-wb-soc
 build_quartus q13.0 /opt/altera/13.0/quartus/bin \
 	de1-picorv32-wb-soc,marsohod2-picorv32-wb-soc,marsohod2bis-picorv32-wb-soc,de1-soc-picorv32-wb-soc
+
+#source /opt/xilinx/Vivado/2016.3/settings64.sh
+#fusesoc --cores-root cores/ build arty
+#RUNMELOG=build/arty_0/bld-vivado/arty_0.runs/synthesis/runme.log
+#( awk '/^***** Vivado/ {seen = 1} seen {print} /^$/ {seen = 0}' $RUNMELOG ; \
+#	awk '/^Report Instance Areas:/ {seen = 1} /^-*$/ {seen = 0} seen {print}' $RUNMELOG ; \
+#	awk '/^Report Cell Usage:/ {seen = 1} seen {print} /^$/ {seen = 0}' $RUNMELOG ) > REPORT.v2016.3
+#mv build build.v2016.3
