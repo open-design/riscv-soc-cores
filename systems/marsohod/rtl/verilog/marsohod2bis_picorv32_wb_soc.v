cb717dbd09da68fabfa9fce585ec543c39805abf
Antony Pavlov <antonynpavlov@gmail.com>
2017-07-19 21:29:18 +0300

    systems: marsohod2bis-picorv32-wb-soc: add gpio
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- systems/marsohod/rtl/verilog/marsohod2bis_picorv32_wb_soc.v
+++ systems/marsohod/rtl/verilog/marsohod2bis_picorv32_wb_soc.v
@@ -50,6 +50,9 @@ assign sdram_dq_i = SDRAM_DQ;
 assign SDRAM_DQ = sdram_dq_oe ? sdram_dq_o : 16'bz;
 assign SDRAM_CLK = sdram_clk;
 
+wire [7:0] gpio0_o;
+assign LED[2:0] = gpio0_o[2:0];
+
 	picorv32_wb_soc #(
 		.BOOTROM_MEMFILE ("../src/riscv-nmon_0/nmon_picorv32-wb-soc_24MHz_115200.txt"),
 		.BOOTROM_MEMDEPTH (1024),
@@ -89,7 +92,11 @@ assign SDRAM_CLK = sdram_clk;
 		.sdram_dq_i		(sdram_dq_i[15:0]),
 		.sdram_dq_oe		(sdram_dq_oe),
 		.sdram_dqm_pad_o	(SDRAM_DQM[1:0]),
-		.sdram_cke_pad_o	()
+		.sdram_cke_pad_o	(),
+
+		.gpio0_i		(),
+		.gpio0_o		(gpio0_o),
+		.gpio0_dir_o		()
 	);
 
 endmodule
