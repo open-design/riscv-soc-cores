cda1649ec6d9359539943f54aa2e28097b4f03b7
Antony Pavlov <antonynpavlov@gmail.com>
2017-07-20 07:49:55 +0300

    systems: de1: sdram: as is
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- systems/de1/rtl/verilog/de1_picorv32_wb_soc.v
+++ systems/de1/rtl/verilog/de1_picorv32_wb_soc.v
@@ -29,8 +29,8 @@ module de1_picorv32_wb_soc(
 		.INPUT_FREQUENCY (50),
 		.DIVIDE_BY (25),
 		.MULTIPLY_BY (12),
-		.C1_DIVIDE_BY (1), // sdram
-		.C1_MULTIPLY_BY (2)
+		.C1_DIVIDE_BY (25), // sdram
+		.C1_MULTIPLY_BY (33)
 	)
 	clkgen(
 		.sys_clk_pad_i(CLOCK_50),
@@ -50,10 +50,24 @@ assign sdram_dq_pad_io = sdram_dq_oe ? sdram_dq_o : 16'bz;
 assign sdram_clk_pad_o = sdram_clk;
 
 	picorv32_wb_soc #(
-		.BOOTROM_MEMFILE ("../src/riscv-nmon_0/nmon_picorv32-wb-soc_24MHz_115200.txt"),
-		.BOOTROM_MEMDEPTH (1024),
-		.SRAM0_MEMFILE ("../src/riscv-nmon_0/memtest.txt"),
-		.SRAM0_MEMDEPTH (16384)
+		.BOOTROM_MEMFILE ("../src/riscv-nmon_0/barebox_nmon_memtest_24_115200_40000000.txt"),
+		.BOOTROM_MEMDEPTH (8192),
+		.SRAM0_MEMDEPTH (16384),
+
+		// ISSI IS42S16400
+		.SDRAM_CLK_FREQ_MHZ	(66),	// sdram_clk freq in MHZ
+		.SDRAM_POWERUP_DELAY	(200),	// power up delay in us
+		.SDRAM_REFRESH_MS	(32),	// time to wait between refreshes in ms
+		.SDRAM_BURST_LENGTH	(8),	// 0, 1, 2, 4 or 8 (0 = full page)
+		.SDRAM_BUF_WIDTH	(3),	// Buffer size = 2^BUF_WIDTH
+		.SDRAM_ROW_WIDTH	(13),	// Row width
+		.SDRAM_COL_WIDTH	(9),	// Column width
+		.SDRAM_BA_WIDTH		(2),	// Ba width
+		.SDRAM_tCAC		(2),	// CAS Latency
+		.SDRAM_tRAC		(5),	// RAS Latency
+		.SDRAM_tRP		(2),	// Command Period (PRE to ACT)
+		.SDRAM_tRC		(7),	// Command Period (REF to REF / ACT to ACT)
+		.SDRAM_tMRD		(2)	// Mode Register Set To Command Delay time
 	)
 	soc(
 		.clock(wb_clk),
