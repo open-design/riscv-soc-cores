6f48f1818c24cc972c6e8a8dbc3ccf3f351fee8d
Antony Pavlov <antonynpavlov@gmail.com>
2017-07-23 12:32:05 +0300

    wb_ram: altsyncram: epic fail
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- cores/wb_ram/rtl/verilog/wb_ram.v
+++ cores/wb_ram/rtl/verilog/wb_ram.v
@@ -1,3 +1,62 @@
+// synopsys translate_off
+`timescale 1 ps / 1 ps
+// synopsys translate_on
+module altsyncram_wrapper #(
+	parameter INIT_FILE
+)
+(
+	input clock,
+	input [15:0] address,
+	input [3:0] byteena,
+	input [31:0] data,
+	input wren,
+	output [31:0] q
+);
+
+	altsyncram	altsyncram_component (
+				.address_a (address),
+				.byteena_a (byteena),
+				.clock0 (clock),
+				.data_a (data),
+				.wren_a (wren),
+				.q_a (q),
+				.aclr0 (1'b0),
+				.aclr1 (1'b0),
+				.address_b (1'b1),
+				.addressstall_a (1'b0),
+				.addressstall_b (1'b0),
+				.byteena_b (1'b1),
+				.clock1 (1'b1),
+				.clocken0 (1'b1),
+				.clocken1 (1'b1),
+				.clocken2 (1'b1),
+				.clocken3 (1'b1),
+				.data_b (1'b1),
+				.eccstatus (),
+				.q_b (),
+				.rden_a (1'b1),
+				.rden_b (1'b1),
+				.wren_b (1'b0));
+	defparam
+		altsyncram_component.byte_size = 8,
+		altsyncram_component.clock_enable_input_a = "BYPASS",
+		altsyncram_component.clock_enable_output_a = "BYPASS",
+		altsyncram_component.init_file = INIT_FILE,
+		altsyncram_component.intended_device_family = "Cyclone V",
+		altsyncram_component.lpm_hint = "ENABLE_RUNTIME_MOD=NO",
+		altsyncram_component.lpm_type = "altsyncram",
+		altsyncram_component.numwords_a = 65536,
+		altsyncram_component.operation_mode = "SINGLE_PORT",
+		altsyncram_component.outdata_aclr_a = "NONE",
+		altsyncram_component.outdata_reg_a = "CLOCK0",
+		altsyncram_component.power_up_uninitialized = "FALSE",
+		altsyncram_component.read_during_write_mode_port_a = "NEW_DATA_NO_NBE_READ",
+		altsyncram_component.widthad_a = 16,
+		altsyncram_component.width_a = 32,
+		altsyncram_component.width_byteena_a = 4;
+endmodule
+
+
 module wb_ram
  #(//Wishbone parameters
    parameter dw = 32,
@@ -56,15 +115,30 @@ module wb_ram
    //TODO:ck for burst address errors
    assign wb_err_o =  1'b0;
 
-   wb_ram_generic
-     #(.depth(depth/4),
-       .memfile (memfile))
-   ram0
-     (.clk (wb_clk_i),
-      .we  ({4{ram_we}} & wb_sel_i),
-      .din (wb_dat_i),
-      .waddr(adr_r[aw-1:2]),
-      .raddr (adr[aw-1:2]),
-      .dout (wb_dat_o));
+//   wb_ram_generic
+//     #(.depth(depth/4),
+//       .memfile (memfile))
+//   ram0
+//     (.clk (wb_clk_i),
+//      .we  ({4{ram_we}} & wb_sel_i),
+//      .din (wb_dat_i),
+//      .waddr(adr_r[aw-1:2]),
+//      .raddr (adr[aw-1:2]),
+//      .dout (wb_dat_o));
+
+	wire [aw - 3:0] wrap_adr;
+	assign wrap_adr = ram_we ? adr_r[aw-1:2] : adr[aw-1:2];
+
+	altsyncram_wrapper #(
+		.INIT_FILE (memfile)
+	)
+	ram0 (
+		.clock(wb_clk_i),
+		.address(wrap_adr),
+		.byteena(wb_sel_i),
+		.data(wb_dat_i),
+		.wren(ram_we),
+		.q(wb_dat_o)
+	);
 
 endmodule
