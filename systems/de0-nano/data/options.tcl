7930b4d82b663b98d4e372ad6b1ce7e40ec9ebcb
Antony Pavlov <antonynpavlov@gmail.com>
2017-08-24 23:54:49 +0300

    systems: add Terasic DE0-Nano board support
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

---
+++ systems/de0-nano/data/options.tcl
@@ -0,0 +1,8 @@
+set_global_assignment -name CYCLONEII_OPTIMIZATION_TECHNIQUE SPEED
+
+set_global_assignment -name GENERATE_CONFIG_SVF_FILE ON
+set_global_assignment -name GENERATE_SVF_FILE ON
+
+set_global_assignment -name GENERATE_RBF_FILE ON
+
+set_global_assignment -name USE_CONFIGURATION_DEVICE ON
