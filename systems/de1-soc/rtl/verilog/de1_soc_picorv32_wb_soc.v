7aacf9994da59ac90ae52e0e0d07db93a12701e3
Antony Pavlov <antonynpavlov@gmail.com>
2017-09-05 20:31:20 +0300

    de1_soc: fixup
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- systems/de1-soc/rtl/verilog/de1_soc_picorv32_wb_soc.v
+++ systems/de1-soc/rtl/verilog/de1_soc_picorv32_wb_soc.v
@@ -1,5 +1,6 @@
 module de1_soc_picorv32_wb_soc(
 	input CLOCK_50,
+
 	output [9:0] LEDR,
 	inout [35:0] GPIO_0
 	);
@@ -17,16 +18,22 @@ module de1_soc_picorv32_wb_soc(
 	wire wb_clk;
 	wire wb_rst;
 
-	altera_pll_clkgen #(
+	altera_pll_wb_clkgen #(
 		.INPUT_FREQUENCY ("50.0 MHz"),
 		.WB_CLK_FREQUENCY ("24.0 MHz")
 	)
 	clkgen(
-		.sys_clk_pad_i(CLOCK_50),
-		.rst_n_pad_i(1),
-		.wb_clk_o(wb_clk),
-		.wb_rst_o(wb_rst)
+		.sys_clk_pad_i (CLOCK_50),
+		.rst_n_pad_i (1),
+		.wb_clk_o (wb_clk),
+		.wb_rst_o (wb_rst)
 	);
+
+	wire uart0_rx;
+	wire uart0_tx;
+
+	assign GPIO_0[1] = uart0_tx;
+	assign uart0_rx = GPIO_0[3];
 
 	wire [7:0] gpio0_o;
 //	assign LEDR[7:0] = gpio0_o[7:0];
@@ -38,19 +45,21 @@ module de1_soc_picorv32_wb_soc(
 	assign gpio0_i[1] = spi0_miso;
 
 	picorv32_wb_soc #(
+		.PROGADDR_RESET (32'h 0000_0000),
+
 		.BOOTROM_MEMFILE ("nmon_picorv32-wb-soc_24MHz_115200.txt"),
 		.BOOTROM_MEMDEPTH (1024)
 	)
 	soc(
-		.clock(wb_clk),
-		.reset(wb_rst),
-		.wb_iadr_o(),
-		.uart_rx(GPIO_0[3]),
-		.uart_tx(GPIO_0[1]),
+		.clock (wb_clk),
+		.reset (wb_rst),
 
-		.gpio0_i		(gpio0_i),
-		.gpio0_o		(gpio0_o),
-		.gpio0_dir_o		()
+		.uart_rx (uart0_rx),
+		.uart_tx (uart0_tx),
+
+		.gpio0_i (gpio0_i),
+		.gpio0_o (gpio0_o),
+		.gpio0_dir_o ()
 	);
 
 endmodule
