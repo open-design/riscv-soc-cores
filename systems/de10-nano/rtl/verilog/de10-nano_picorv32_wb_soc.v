a4cd45c2fee23f93b124bc6604de88cb3e2fabda
Antony Pavlov <antonynpavlov@gmail.com>
2017-09-05 22:54:53 +0300

    de10-nano: sync w de1-soc
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- systems/de10-nano/rtl/verilog/de10-nano_picorv32_wb_soc.v
+++ systems/de10-nano/rtl/verilog/de10-nano_picorv32_wb_soc.v
@@ -2,7 +2,7 @@ module de10_nano_picorv32_wb_soc(
 	input CLOCK_50,
 	input [1:0] KEY,
 	output [7:0] LED,
-	inout [7:0] GPIO_0
+	inout [7:0] GPIO_0,
 	inout [7:0] GPIO_1
 	);
 
@@ -29,41 +29,49 @@ module de10_nano_picorv32_wb_soc(
 	wire wb_clk;
 	wire wb_rst;
 
-	altera_pll_clkgen #(
+	altera_pll_wb_clkgen #(
 		.INPUT_FREQUENCY ("50.0 MHz"),
-		.OUTPUT_CLOCK0_FREQUENCY ("24.0 MHz")
+		.WB_CLK_FREQUENCY ("24.0 MHz")
 	)
 	clkgen(
-		.sys_clk_pad_i(CLOCK_50),
-		.rst_n_pad_i(1),
-		.wb_clk_o(wb_clk),
-		.wb_rst_o(wb_rst)
+		.sys_clk_pad_i (CLOCK_50),
+		.rst_n_pad_i (1),
+		.wb_clk_o (wb_clk),
+		.wb_rst_o (wb_rst)
 	);
 
-wire [7:0] gpio0_o;
-//assign LED[7:0] = gpio0_o[7:0];
+	wire uart0_rx;
+	wire uart0_tx;
+
+	assign GPIO_0[1] = uart0_tx;
+	assign uart0_rx = GPIO_0[3];
+
+	wire [7:0] gpio0_o;
+	//assign LED[7:0] = gpio0_o[7:0];
 	assign spi0_sck = gpio0_o[0];
 	assign spi0_mosi = gpio0_o[2];
 	assign spi0_cs0 = gpio0_o[3];
 
-wire [7:0] gpio0_i;
-//assign gpio0_i[1:0] = KEY[1:0];
+	wire [7:0] gpio0_i;
+	//assign gpio0_i[1:0] = KEY[1:0];
 	assign gpio0_i[1] = spi0_miso;
 
 	picorv32_wb_soc #(
-		.BOOTROM_MEMFILE ("../src/riscv-nmon_0/nmon_picorv32-wb-soc_24MHz_115200.txt"),
+		.PROGADDR_RESET (32'h 0000_0000),
+
+		.BOOTROM_MEMFILE ("nmon_picorv32-wb-soc_24MHz_115200.txt"),
 		.BOOTROM_MEMDEPTH (1024)
 	)
 	soc(
-		.clock(wb_clk),
-		.reset(wb_rst),
-		.wb_iadr_o(),
-		.uart_rx(GPIO_0[3]),
-		.uart_tx(GPIO_0[1]),
+		.clock (wb_clk),
+		.reset (wb_rst),
 
-		.gpio0_i		(gpio0_i),
-		.gpio0_o		(gpio0_o),
-		.gpio0_dir_o		()
+		.uart_rx (uart0_rx),
+		.uart_tx (uart0_tx),
+
+		.gpio0_i (gpio0_i),
+		.gpio0_o (gpio0_o),
+		.gpio0_dir_o ()
 	);
 
 endmodule
