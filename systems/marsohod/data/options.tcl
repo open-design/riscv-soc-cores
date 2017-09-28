c67bdc84c0f67a0250264865ecf10c2634c16ffa
Antony Pavlov <antonynpavlov@gmail.com>
2017-09-28 17:19:47 +0300

    systems: marsohod: generate RBF file
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- systems/marsohod/data/options.tcl
+++ systems/marsohod/data/options.tcl
@@ -5,3 +5,5 @@ set_global_assignment -name AUTO_RAM_BLOCK_BALANCING OFF
 
 set_global_assignment -name GENERATE_CONFIG_SVF_FILE ON
 set_global_assignment -name GENERATE_SVF_FILE ON
+
+set_global_assignment -name GENERATE_RBF_FILE ON
