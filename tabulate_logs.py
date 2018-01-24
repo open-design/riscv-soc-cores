d3417d7b0316fa66c31118616d144ffb410f1073
Antony Pavlov <antonynpavlov@gmail.com>
2018-01-24 21:32:09 +0300

    tabulate_logs.py: add marsohod2rpi board
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- tabulate_logs.py
+++ tabulate_logs.py
@@ -149,7 +149,8 @@ data = []
 for (quartuss, boards) in (
         (("13.0",), ("core-ep2c5", "de1")),
         (("13.1",), ("marsohod2",)),
-        (("13.1", "17.1"), ("marsohod2bis", "core-ep4ce6", "de0-nano")),
+        (("13.1", "17.1"), ("marsohod2bis", "marsohod2rpi",
+                            "core-ep4ce6", "de0-nano")),
         (("17.1",), ("marsohod3", "marsohod3bis", "c10lp-evkit")),
                         ):
 
