4c3ab3230f03c85dc927caa07ebd35fc7ab04d61
Antony Pavlov <antonynpavlov@gmail.com>
2018-01-24 21:34:27 +0300

    tabulate_logs.py: analyze marsohod2 quartus-13.0 logs
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- tabulate_logs.py
+++ tabulate_logs.py
@@ -148,7 +148,7 @@ class quartus_rpt():
 data = []
 for (quartuss, boards) in (
         (("13.0",), ("core-ep2c5", "de1")),
-        (("13.1",), ("marsohod2",)),
+        (("13.0", "13.1",), ("marsohod2",)),
         (("13.1", "17.1"), ("marsohod2bis", "marsohod2rpi",
                             "core-ep4ce6", "de0-nano")),
         (("17.1",), ("marsohod3", "marsohod3bis", "c10lp-evkit")),
